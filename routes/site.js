193fa8c31e05954c274818487b717ae897616b17
kirran01 <Kirran-@live.com>
2022-11-26 20:18:47 -0500

    login added

--- routes/site.js
+++ routes/site.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const axios = require("axios");
 const bcryptjs = require("bcryptjs");
+const session = require("express-session");
 const User = require("../models/User.model");
 
 //GET HOME
@@ -27,14 +28,18 @@ router.get("/signup", (req, res) => {
 
 //POST SIGNUP
 router.post("/signup", (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    res.send("field(s) are blank");
+    return;
+  }
   User.create({
     email: req.body.email,
     userName: req.body.userName,
     password: bcryptjs.hashSync(req.body.password),
   })
     .then((newUser) => {
-      console.log(newUser,"<-- new user");
-      res.redirect("/")
+      console.log(newUser, "<-- new user");
+      res.redirect("/");
     })
     .catch((err) => {
       res.send(err);
@@ -44,6 +49,36 @@ router.post("/signup", (req, res) => {
 //GET LOGIN
 router.get("/login", (req, res) => {
   res.render("login.hbs");
+});
+
+//POST LOGIN
+router.post("/login", (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    res.send("field(s) are blank");
+    return;
+  }
+  User.findOne({ email: req.body.email })
+    .then((foundUser) => {
+      if (!foundUser) {
+        res.send("user does not exist");
+        return;
+      }
+      const isValidPassword = bcryptjs.compareSync(
+        req.body.password,
+        foundUser.password
+      );
+      if (!isValidPassword) {
+        res.send("password incorrect");
+        return;
+      }
+      //   session is for cookies
+      console.log(foundUser, "<--foundUser");
+      req.session.user = foundUser;
+      res.render("home.hbs", foundUser);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 });
 
 module.exports = router;
